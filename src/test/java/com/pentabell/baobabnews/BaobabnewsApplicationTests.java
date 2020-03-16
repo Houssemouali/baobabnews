d2d5838066bf90609a49de8ddd15945c9c74589a
houssem2 <houssem.ouali@esprit.tn>
2020-03-16 11:41:49 +0100

    updates

---
+++ src/test/java/com/pentabell/baobabnews/BaobabnewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pentabell.baobabnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaobabnewsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
